b7841d40d078758af7e3679d04c28805402640a8
Orlando Jose Umanzor Zelaya <orlandojose729@gmail.com>
2022-08-17 17:45:30 -0600

    Fix minor bug when input is 0

--- Fibonacci/script.js
+++ Fibonacci/script.js
@@ -7,15 +7,12 @@ function fibs(n){
 }
 
 function fibsRec(n){
+   if(n === 0) return 'INVALID NUM'
    if (n == 1) return [0]
    else if (n == 2) return [0, 1]
 
    let lastFib = fibsRec(n - 1);
    return lastFib.concat(lastFib[lastFib.length - 1] + lastFib[lastFib.length - 2])
 }
-const fibRecursive = (n, a = [0, 1]) => {
-    if (a.length >= n) return a;
-    return fibRecursive(n, [...a, a[a.length - 2] + a[a.length - 1]]);
-  };
 
-console.log(fibRecursive(0))
+console.log(fibsRec(8))
